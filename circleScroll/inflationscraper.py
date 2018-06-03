f71f803ec2bdd5f1e3d12d616c59e66dd2549e57
mrachamallu <mrachamallu@gmail.com>
2018-06-02 21:23:19 -0700

    successfully scrapes inflation value

--- circleScroll/inflationscraper.py
+++ circleScroll/inflationscraper.py
@@ -1,3 +1,15 @@
 # import libraries
-import urllib2
 from bs4 import BeautifulSoup
+import requests
+
+# specify the url
+quote_page = requests.get('http://www.usinflationcalculator.com/inflation/current-inflation-rates/')
+
+# parse the html using beautiful soup and store in variable `soup`
+soup = BeautifulSoup(quote_page.text, 'html.parser')
+
+# Take out the <div> of name and get its value
+data = soup.find('aside', {'id': 'text-176031101'})
+data = data.find('u')
+data = data.text
+print(data)
